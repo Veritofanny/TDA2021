95e5a2dbffd46f103612ca13e717accc0fe7d6f6
CLINTONARMANDO <clintonarmando25@gmail.com>
2021-06-10 22:56:36 -0500

    elavoracion de dashboard

---
+++ app/Controllers/da.php
@@ -0,0 +1,31 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Da extends BaseController
+{
+	public function index()
+	{
+		echo view('index').view('Vistaindex');
+	}
+	public function tablero()
+	{
+		echo view('index').view('Vistatablero');
+	}
+	public function estadisticas()
+	{
+		echo view('index').view('Vistaestadisticas');
+	}
+	public function perfil()
+	{
+		echo view('index').view('Vistaperfil');
+	}
+	public function configuraciones()
+	{
+		echo view('index').view('Vistaconfiguraciones');
+	}
+}
+
+
+	//--------------------------------------------------------------------
+
