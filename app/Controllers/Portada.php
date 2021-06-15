633028cdbdce930b2fe49fb8441c3fd0d98afd83
Edwin <mcedwin@gmail.com>
2021-06-15 07:34:11 -0500

    mod portada

--- app/Controllers/Portada.php
+++ app/Controllers/Portada.php
@@ -6,7 +6,7 @@ class Portada extends BaseController
 {
 	public function index()
 	{
-        echo view('header');
+        echo view('header2');
 		echo view('portada');
         echo view('footer');
 	}
