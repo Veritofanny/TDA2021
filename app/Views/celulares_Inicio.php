4c5b4d30c45bbb33a1ecf61a344537487b7ed60a
unknown <sempairent@gmail.com>
2021-06-24 12:47:43 -0500

    actu

---
+++ app/Views/celulares_Inicio.php
@@ -0,0 +1,235 @@
+<br>
+<style>
+body {
+    background-image: url("https://images4.alphacoders.com/876/thumb-1920-87656.jpg");
+    
+}
+h1{
+  color:white;
+}
+</style>
+<br>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://lh3.googleusercontent.com/ggtbLxlgMevvgXKqOuzTanWQAb88UBoJoS_oDn5OFFUXl3gVujVBh6oRlMSg3HwIVTLW=h1024-no-tmp_theme_for_huawei_y9_prime_wallpaper_launcher_y9_apk.jpg" class="d-block w-100" alt="..." width="420" height="620">
+          </div>
+          <div class="carousel-item">
+            <img src="https://media.cdnp.elobservador.com.uy/102018/1539787804371.png?cw=1131&ch=578" class="d-block w-100" alt="..." width="420" height="620">
+          </div>
+          <div class="carousel-item">
+            <img src="https://i.blogs.es/ba79e6/huawei-p40-lite-review-xataka-portada/1366_2000.jpg" class="d-block w-100" alt="..." width="420" height="620">
+          </div>
+          <div class="carousel-item">
+            <img src="http://agendatecnologicaweb.com/wp-content/uploads/1090.jpg" class="d-block w-100" alt="..." width="420" height="620">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <br><br><br>
+
+
+
+
+      <h1>CATALOGO</h1>
+      <br>
+
+
+
+
+<div class="card-group ">
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/fa9908fd-3e6c-4783-8dea-857e7dae8c7b/xiaomi-redmi9a-azul.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-fa9908fd-3e6c-4783-8dea-857e7dae8c7b-nEvRw93" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Xiaomi Redmi 9A 32GB Azul</h5>
+      <p class="card-text">¡Déjate sorprender por el Xiaomi Redmi 9A! Este equipo cuenta con todo lo que estabas esperando: buena cámara, batería y un excelente diseño. Descubre todo lo que puedes hacer con los increíbles efectos del caleidoscopio y añade diversión a todos tus videos.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.589</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/79952980-c700-4775-9426-86fc4283b59c/Y9a-400-x-400AAAA.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-79952980-c700-4775-9426-86fc4283b59c-nEl3Ol-" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Huawei Y9a 128GB Negro</h5>
+      <p class="card-text">Cámbiate o renueva con Claro y llévate a casa el nuevo Huawei Y9a. Disfruta de la experiencia Huawei gracias a su potente dispositivo con su procesador de 8 núcleos y 6 GB de memoria RAM. Ejecuta las aplicaciones que quieras y haz multitareas con total fluidez. Compra ahora el nuevo Huawei Y9A en Perú a un excelente precio.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1299 </h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/b9c32a2f-f8e0-4819-96cb-16ccf555feb8/CyberGMoto-G9Power_STARS.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-b9c32a2f-f8e0-4819-96cb-16ccf555feb8-nBmtDJO" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Motorola Moto G9 Power 128GB Verde</h5>
+      <p class="card-text">El Moto G9 Power es ligero y elegante, con el tamaño perfecto para usarlo con una sola mano. Su pantalla es Max Vision de 6.8 pulgadas con una resolución HD+ y tiene una relación de aspecto de 20.5:9, una vista súper amplia para conseguir la máxima experiencia sin importar qué estés mirando. Disfruta de series, películas, juegos, y más. </p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.809</h4></small></p>
+    </div>
+  </div>
+</div>
+<h1>.</h1>
+<div class="card-group">
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/bd8d52ae-8055-4975-9180-9a1126c96b4e/Vista_frontal_GalaxyA32.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-bd8d52ae-8055-4975-9180-9a1126c96b4e-nEbTXDH" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Samsung Galaxy A32 128 GB Negro</h5>
+      <p class="card-text">El Galaxy A32 cuenta con una pantalla Infinity-U Super AMOLED de 6.4 pulgadas a resolución Full HD+, y potenciado por un procesador Mediatek Helio G80 con opciones de 4GB de memoria RAM Y 128GB de almacenamiento interno expandible. En cuanto a cámaras, el Galaxy A32 cuenta con una cámara cuádruple en su posterior, con lentes de 64MP, 8MP, 5MP y 5MP, y su cámara frontal para selfies es de 20 megapixels.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.949</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/1c59b356-f7e5-42e7-bb79-acabec2d0ddc/vista_400x400_Galaxy_A52dd09666.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-1c59b356-f7e5-42e7-bb79-acabec2d0ddc-nDAeUdP" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Samsung Galaxy A52 128GB Negro</h5>
+      <p class="card-text">Disfruta de los detalles realmente vibrantes con la pantalla Super AMOLED Display FHD+, que alcanza los 800 nits¹ para mayor claridad, incluso con la luz de día brillante. Protector para la comodidad de los ojos² reduce la luz azul y la pantalla extrafluida mantiene la vista relajada, ya sea que estés jugando o navegando. Todo en la amplia pantalla Infinity-O Display de 6,5 pulgadas.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1499</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/8ee70e48-7ead-4176-ac32-f1d99ae794ee/_HUA-Y7A-64GB-VR+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-8ee70e48-7ead-4176-ac32-f1d99ae794ee-nEl5ewo" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Huawei Y7a 64GB Negro</h5>
+      <p class="card-text">Vive la mejor experiencia sin tener miedo a quedarte en “0” gracias a su potente batería de 5000 mAh con carga rápida. Además, con su cámara frontal de 8 MP y sus 4 cámaras posteriores siendo la principal de 48 MP, todas con inteligencia artificial, podrás capturar los mejores momentos de tu vida con la palma de tu mano. Compra ya el Huawei Y7a de 64 GB en Perú a un excelente precio. </p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.769</h4></small></p>
+    </div>
+  </div>
+</div>
+<h1>.</h1>
+
+<div class="card-group">
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/92fb323e-9c84-41e1-af05-b7759a0ae78e/IPHONE+11+pro_frontal_.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE-92fb323e-9c84-41e1-af05-b7759a0ae78e-nqFJ0Qb" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Apple iPhone 11 PRO 512GB Dorado</h5>
+      <p class="card-text">Phone 11 Pro 512 GB de Apple. Cuenta con una memoria RAM de 4GB y el chip A13 Bionic, uno de los más rápidos que ha lanzado Apple en su historia, con el que podrás ejecutar todo tipo de aplicaciones sin problemas. Compra ahora el nuevo iPhone 11 PRO en Perú a un excelente precio. </p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.5169</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/a5dcd085-10db-4415-a76a-4f11e07757f5/RedmiNote10Pro_400x400_frontaldddd.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-a5dcd085-10db-4415-a76a-4f11e07757f5-nEQDDll" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Xiaomi Redmi Note 10 Pro 128GB Azul</h5>
+      <p class="card-text">El Xiaomi Redmi Note 10 Pro es un dispositivo para satisfacer altas demandas y cumplir con la expectativa de todos. Ofrece un sistema de cámara cuádruple con un sensor principal de 108 megapíxeles para realizar fotografías en alta resolución. Tiene un procesador Snapdragon 732G y 6GB de memoria RAM para realizar tareas con total fluidez. Compra el nuevo Redmi Note 10 Pro en Perú a un excelente precio.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1159</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card border-dark gray">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/d22f551a-e3bb-4912-a42d-bf60fde01d39/SammSw2ssss0frontal.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-d22f551a-e3bb-4912-a42d-bf60fde01d39-nD7WbOm" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Samsung Galaxy S20 FE 128GB Azul</h5>
+      <p class="card-text"> El Samsung Galaxy S20 Fan Edition ofrece varias opciones en cuanto a colores, además su acabado lo hace ver elegante. Asimismo, con sus 4 curvaturas a los lados le permite a cualquiera mantener un mejor agarre. Su pantalla es una SUPER AMOLED de 6.5 pulgadas full HD que ofrece una tasa de refrescamiento de 120Hz, de esa manera tendrás una mejor experiencia de navegación sin tanta vibración.  </p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.2059</h4></small></p>
+    </div>
+  </div>
+</div>
+<br>
+<h1>Accesorios</h1>
+      <br>
+      <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/4b940e7c-e6a8-406f-9630-b1f620424b45/Apple_Watch_Gold_1+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-4b940e7c-e6a8-406f-9630-b1f620424b45-nibamYM" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Apple Watch Series 5 40mm Gold</h5>
+      <p class="card-text">Los Apple Watch Series 5 cuenta con Altímetro barométrico, Sensor eléctrico de frecuencia cardiaca, Sensor de luz ambienta, Acelerómetro, Brújula, Giroscopio, Cubierta trasera de cerámica y cristal de zafiro y tienen un grado de resistencia al agua  Sin embargo, no deben usarse para practicar buceo, esquí acuático ni actividades que conlleven impactos de agua a gran velocidad o inmersiones profundas.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1549</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/a4010c38-7b6b-4627-a842-285cc2289a85/Apple_Watch_Silver_1+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-a4010c38-7b6b-4627-a842-285cc2289a85-nibavMp" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Apple Watch Series 5 40mm Silver</h5>
+      <p class="card-text">Los Apple Watch Series 5 cuenta con Altímetro barométrico, Sensor eléctrico de frecuencia cardiaca, Sensor de luz ambienta, Acelerómetro, Brújula, Giroscopio, Cubierta trasera de cerámica y cristal de zafiro y tienen un grado de resistencia al agua  Sin embargo, no deben usarse para practicar buceo, esquí acuático ni actividades que conlleven impactos de agua a gran velocidad o inmersiones profundas.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1430</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/4e2d9fe7-507a-42f3-9a7c-f0cd306edc4e/Apple_Watch_Black_1+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-4e2d9fe7-507a-42f3-9a7c-f0cd306edc4e-niba.-O" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Apple Watch Series 5 44mm Space Gray</h5>
+      <p class="card-text">Los Apple Watch Series 5 cuenta con Altímetro barométrico, Sensor eléctrico de frecuencia cardiaca, Sensor de luz ambienta, Acelerómetro, Brújula, Giroscopio, Cubierta trasera de cerámica y cristal de zafiro y tienen un grado de resistencia al agua  Sin embargo, no deben usarse para practicar buceo, esquí acuático ni actividades que conlleven impactos de agua a gran velocidad o inmersiones profundas.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.1649</h4></small></p>
+    </div>
+  </div>
+</div>
+
+<h1>.</h1>
+
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/9d5ff71b-ef60-4f94-bc0d-920c57a1861a/-Huawei-BAND4E-Gris+copia.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-9d5ff71b-ef60-4f94-bc0d-920c57a1861a-nBHSK61" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Huawei Band 4e Gris</h5>
+      <p class="card-text">Mantén controlada tu actividad física gracias a la Huawei Band 4e. Posee un sensor de movimiento de 6 ejes que, sumado a su innovadora tecnología, la banda te proporcionará hasta 9 métricas de forma de carrera donde tiene un patrón de pisada, impacto de aterrizaje, tiempo de contacto con el suelo y mucho más.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.89</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/c05d580b-b8c5-4ab2-8a8c-2bf6ae926e7c/0-Huawei-BAND4E-Rosa+copia.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-c05d580b-b8c5-4ab2-8a8c-2bf6ae926e7c-nBHTjcb" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Huawei Band 4e Rosa</h5>
+      <p class="card-text">Mantén controlada tu actividad física gracias a la Huawei Band 4e. Posee un sensor de movimiento de 6 ejes que, sumado a su innovadora tecnología, la banda te proporcionará hasta 9 métricas de forma de carrera donde tiene un patrón de pisada, impacto de aterrizaje, tiempo de contacto con el suelo y mucho más.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.89</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/a68f056b-442a-40b3-9e89-f18984061198/audio_negro3a+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-a68f056b-442a-40b3-9e89-f18984061198-nidfp-W" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Skullcandy Audífonos Bluetooth Hesh 3 Wireless Negro</h5>
+      <p class="card-text">Los Wireless o Audífonos ScullCandy Hesh 3 ofrecen todo lo necesario para disfrutar al máximo del buen sonido gracias a su característica de aislar el ruido exterior de manera efectiva. Esto es posible ya que, en su composición, trae unas almohadillas especiales que logran cubrir las orejas y trabajar como un aislante natural.</p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.229</h4></small></p>
+    </div>
+  </div>
+</div>
+
+<h1>.</h1>
+
+
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/04679621-8e07-47a0-8136-2acbdcdc1de6/pus1verd+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-04679621-8e07-47a0-8136-2acbdcdc1de6-nidOhzr  " alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Skullcandy Audífonos Push True Wireless Earbuds V</h5>
+      <p class="card-text"></p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.229</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/3b220566-3779-4672-bfab-6ceb56f40497/pus1grisa+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-3b220566-3779-4672-bfab-6ceb56f40497-nidNEoJ" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Skullcandy Audífonos Push True Wireless Earbuds N</h5>
+      <p class="card-text"></p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.299</h4></small></p>
+    </div>
+  </div>
+  <h1>.</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://tiendaclaro.pe/wcm/connect/60b973e4-0cfa-46c0-ac70-89113b01e467/twe2plomo+%281%29+%281%29.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-60b973e4-0cfa-46c0-ac70-89113b01e467-niecAAF" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">BlackSheep Audífonos Bluetooth In Ear TWEAKER G</h5>
+      <p class="card-text"></p>
+      <p class="card-text"><small class="text-muted"><h4>PRECIO S/.99</h4></small></p>
+    </div>
+  </div>
+</div>
+<br><br>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
