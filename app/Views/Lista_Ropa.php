42103292178da8fdf671de7eeb726e9fe90ca1db
jefer-apaza <jeferapazaa@gmail.com>
2021-06-25 05:45:02 -0500

    Terminado

--- app/Views/Lista_Ropa.php
+++ app/Views/Lista_Ropa.php
@@ -1,69 +1,62 @@
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-    <h4>
-        Tabla de productos:
-    </h4>
-    
-    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        <i class="far fa-plus-square"></i>  Crear
-    </button>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <h4>
+            Tabla de Ropas en el almacen
+        </h4>
+        
+       
+        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <i class="far fa-plus-square"></i>  Insertar nueva ropa recibida
+        </button>
+
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Formulario de cuestionario</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="row g-3">
-                        <div class="col-md-4">
-                            <label for="validationDefault01" class="form-label"><i class="fab fa-slack-hash"></i> Nombre</label>
-                            <input type="text" class="form-control" id="validationDefault01" placeholder="Gaseosa" required>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="validationDefault02" class="form-label"><i class="far fa-registered"></i> Marca </label>
-                            <input type="text" class="form-control" id="validationDefault02" placeholder="Coca Cola" required>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="validationDefaultUsername" class="form-label"><i class="fas fa-hourglass-end"></i> Contacto</label>
-                            <div class="input-group">
-                                <span class="input-group-text" id="inputGroupPrepend2">@</span>
-                                <input type="text" class="form-control" id="validationDefaultUsername" placeholder="ejmplo@gmail.com" aria-describedby="inputGroupPrepend2" required>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault03" class="form-label"><i class="fab fa-slack-hash"></i> Precio (S/.)</label>
-                            <input type="text" class="form-control" id="validationDefault03" required placeholder=1,00">
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault03" class="form-label"><i class="fas fa-truck-loading"></i> Cantidad</label>
-                            <input type="text" class="form-control" id="validationDefault03" required placeholder="100 ml - 6 Unidades">
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault05" class="form-label"><i class="fas fa-hourglass-start"></i> Fecha de emision</label>
-                            <input type="text" class="form-control" id="validationDefault05" required placeholder="12-12-12">
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault05" class="form-label"> <i class="fas fa-hourglass-end"></i> Fecha de vencimiento</label>
-                            <input type="text" class="form-control" id="validationDefault05" required placeholder="11-11-11">
-                        </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-                                <label class="form-check-label" for="invalidCheck2">
-                                Marcar como Ropa de Temporada
-                                </label>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-">Guardar producto</button>
-                </div>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Formulario de ingreso al almacen</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <form class="row g-3">
+            <div class="col-md-10">
+                <label for="validationDefault01" class="form-label"><i class=" text-danger fab fa-slack-hash"></i> Nombre de la Ropa o Accesorio</label>
+                <input type="text" class="form-control" id="validationDefault01" placeholder="Gorro de sol" >
+            </div>
+            <div class="col-md-4">
+                <label for="validationDefault02" class="form-label"><i class="text-danger far fa-registered"></i> Marca</label>
+                <input type="text" class="form-control" id="validationDefault02" placeholder="Gamarra INC" >
+            </div>
+            <div class="col-md-3">
+                <label for="validationDefault03" class="form-label"><i class="text-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+  <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+</svg></i> Precio Unidad (S/.)</label>
+                <input type="text" class="form-control" id="validationDefault03" placeholder="100.00">
+            </div>
+            
+            <div class="col-md-3">
+                <label for="validationDefault03" class="form-label"><i class="text-danger fas fa-truck-loading"></i> Fecha de Llegada</label>
+                <input type="text" class="form-control" id="validationDefault03"  placeholder="20-05-2010">
+            </div>
+            <div class="col-md-3">
+                <label for="validationDefault03" class="form-label"><i class="text-danger fas fa-truck-loading"></i> Stock de Llegada</label>
+                <input type="text" class="form-control" id="validationDefault03"  placeholder="600 Unidades">
+            </div>
+            <div class="col-md-3">
+                <label for="validationDefault05" class="form-label"><i class="text-danger fas fa-hourglass-start"></i> Stock Actual</label>
+                <input type="text" class="form-control" id="validationDefault05"  placeholder="300 Unidades">
+            </div>
+            
+            </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-danger">Guardar producto</button>
+            </div>
             </div>
         </div>
-    </div>
+        </div>
+
 </div>
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
@@ -401,6 +394,12 @@
                     <td>900 Unidades</td>
                     <td>100 Unidades</td>
                 </tr>
+                
             </tbody>
         </table>
 </div>
+<script>
+    $(document).ready (function(){
+        $('#example').DataTable();
+    });
+</script>
